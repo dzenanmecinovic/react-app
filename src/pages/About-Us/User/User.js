7ad72f0a706192aaf76ff5dce4838c0059c84707
Dzenan Mecinovic <mecinovicd@gmail.com>
2023-05-15 21:36:54 +0200

    react

--- src/pages/About-Us/User/User.js
+++ src/pages/About-Us/User/User.js
@@ -1,28 +1,11 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import UserOne from "./UserOne";
-import { BASE_URL } from "../../../config/api";
 import { AppContext } from "../../../context/AppContext";
-import axios from "axios";
 
 export default function User() {
   const { id } = useParams();
-  const { data, setData } = useContext(AppContext);
-
-  async function getData() {
-    try {
-      const users = await axios.get(`${BASE_URL}/users`);
-      const usersData = await users.data;
-      const nizObjekata = Object.values(usersData).flat();
-      setData(nizObjekata.slice(1, 5));
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  useEffect(() => {
-    getData();
-  }, []);
+  const { data } = useContext(AppContext);
   const user = data.find((e) => {
     return e.id == id;
   });
